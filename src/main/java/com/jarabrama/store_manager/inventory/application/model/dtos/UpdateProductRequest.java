2a9862ab38affbf39fa64e560cf9dd4150fcb69d
brayan <brayanjaraba2006@gmail.com>
2026-06-17 09:44:02 -0500

    feat: add data validation to update product request

--- src/main/java/com/jarabrama/store_manager/inventory/application/model/dtos/UpdateProductRequest.java
+++ src/main/java/com/jarabrama/store_manager/inventory/application/model/dtos/UpdateProductRequest.java
@@ -1,12 +1,32 @@
 package com.jarabrama.store_manager.inventory.application.model.dtos;
 
+import jakarta.validation.constraints.PositiveOrZero;
+import jakarta.validation.constraints.Size;
 import java.util.List;
 
 public record UpdateProductRequest(
+  @Size(
+    max = 50,
+    message = "El nombre de un producto no puede tener mas de 50 caracteres"
+  )
   String name,
+
+  @Size(
+    max = 100,
+    message = "La descripcion del producto no puede tener mas de 100 caracteres"
+  )
   String description,
+
+  @PositiveOrZero(
+    message = "El precio de un producto debe ser mayor o igual a cero"
+  )
   double price,
+
+  @PositiveOrZero(
+    message = "Las unidades disponibles deben ser mayor o igual a cero"
+  )
   int stock,
+  
   String imageUrl,
   List<String> categories
 ) {}
