f919d3623bf9f489dc4949c2ba1e8826d85346e6
brayan <brayanjaraba2006@gmail.com>
2026-07-27 15:57:31 -0500

    chore: optimize imports

--- src/main/java/com/jarabrama/store_manager/configuration/CorsConfig.java
+++ src/main/java/com/jarabrama/store_manager/configuration/CorsConfig.java
@@ -1,10 +1,7 @@
 package com.jarabrama.store_manager.configuration;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
