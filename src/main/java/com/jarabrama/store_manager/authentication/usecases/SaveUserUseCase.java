48a2dfab3182a1a287b11a9427014dd46ee2de32
brayan <brayanjaraba2006@gmail.com>
2026-07-30 07:19:29 -0500

    feat: implement verification of existence of the user name and make transactional the execute function

--- src/main/java/com/jarabrama/store_manager/authentication/usecases/SaveUserUseCase.java
+++ src/main/java/com/jarabrama/store_manager/authentication/usecases/SaveUserUseCase.java
@@ -2,12 +2,14 @@ package com.jarabrama.store_manager.authentication.usecases;
 
 import com.jarabrama.store_manager.authentication.domain.SystemUserRepositoryPort;
 import com.jarabrama.store_manager.authentication.domain.exceptions.InvalidNewUserException;
+import com.jarabrama.store_manager.authentication.domain.exceptions.UserAlreadyExistsException;
 import com.jarabrama.store_manager.authentication.domain.model.SystemRole;
 import com.jarabrama.store_manager.authentication.domain.model.SystemUser;
 import com.jarabrama.store_manager.authentication.presentation.dto.NewUserRequest;
 import lombok.AllArgsConstructor;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @AllArgsConstructor
 @Service
@@ -16,9 +18,11 @@ public class SaveUserUseCase {
   private final SystemUserRepositoryPort userRepository;
   private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+  @Transactional
   public String execute(NewUserRequest newUser) {
     validateUserName(newUser);
     validateUserPassword(newUser);
+    validateUserNameAlreadyExits(newUser.username());
 
     var passHash = bCryptPasswordEncoder.encode(newUser.password());
     var user = SystemUser.builder().role(SystemRole.EMPLOYEE)
@@ -28,6 +32,13 @@ public class SaveUserUseCase {
 
     userRepository.save(user);
     return "Usuario creado exitosamente";
+  }
+
+  private void validateUserNameAlreadyExits(String username) {
+    var optional = userRepository.findByUsername(username);
+    if (optional.isPresent()) {
+      throw new UserAlreadyExistsException("El nombre de usuario ya existe");
+    }
   }
 
   private void validateUserName(NewUserRequest newUser) {
