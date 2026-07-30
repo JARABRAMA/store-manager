3ac40584a5f36580f70ad19e28633731db776c1c
brayan <brayanjaraba2006@gmail.com>
2026-07-30 06:59:40 -0500

    feat: implement system user controller

---
+++ src/main/java/com/jarabrama/store_manager/authentication/presentation/controllers/SystemUserController.java
@@ -0,0 +1,26 @@
+package com.jarabrama.store_manager.authentication.presentation.controllers;
+
+import com.jarabrama.store_manager.authentication.presentation.dto.NewUserRequest;
+import com.jarabrama.store_manager.authentication.usecases.SaveUserUseCase;
+import com.jarabrama.store_manager.inventory.application.model.dtos.SimpleResponse;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+@AllArgsConstructor
+public class SystemUserController {
+  private final SaveUserUseCase saveUserUseCase;
+
+  @PostMapping()
+  public ResponseEntity<SimpleResponse> save(@RequestBody NewUserRequest req) {
+    var message = saveUserUseCase.execute(req);
+    return new ResponseEntity(new SimpleResponse(message), HttpStatus.CREATED);
+  }
+}
