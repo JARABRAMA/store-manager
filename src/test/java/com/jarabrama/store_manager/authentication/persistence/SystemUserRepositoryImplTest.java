a291eedcdb3b388ee19b2b17402ba73cca85436a
brayan <brayanjaraba2006@gmail.com>
2026-07-30 13:56:14 -0500

    test: find by username repository

--- src/test/java/com/jarabrama/store_manager/authentication/persistence/SystemUserRepositoryImplTest.java
+++ src/test/java/com/jarabrama/store_manager/authentication/persistence/SystemUserRepositoryImplTest.java
@@ -1,16 +1,11 @@
 package com.jarabrama.store_manager.authentication.persistence;
 
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.doThrow;
-import static org.mockito.Mockito.when;
-
 import com.jarabrama.store_manager.authentication.domain.exceptions.DatabaseException;
 import com.jarabrama.store_manager.authentication.domain.model.SystemRole;
 import com.jarabrama.store_manager.authentication.domain.model.SystemUser;
 import com.jarabrama.store_manager.authentication.persistence.entities.SystemUserEntity;
 import com.jarabrama.store_manager.authentication.persistence.jpa.SystemUserJpaRepository;
 import com.jarabrama.store_manager.authentication.persistence.mappers.SystemUserEntityMapper;
-import kotlin._Assertions;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -18,8 +13,12 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.Spy;
-import org.mockito.internal.matchers.Any;
-import org.springframework.dao.DataIntegrityViolationException;
+
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
 
 /**
  * SystemUserRepositoryImplTest
@@ -59,5 +58,31 @@ public class SystemUserRepositoryImplTest {
   void saved_successfully_user_by_jpa() {
     when(jpaRepository.save(any(SystemUserEntity.class))).thenReturn(SystemUserEntity.builder().build());
     repository.save(validUser); // should do nothing
+  }
+
+  @Test
+  void find_by_username_did_not_found_anything() {
+    var username = "admin";
+    when(jpaRepository.findByUsername(username)).thenReturn(Optional.empty());
+    Assertions.assertEquals(Optional.empty(), repository.findByUsername(username));
+  }
+
+  @Test
+  void find_by_username_found_an_user() {
+    var entity = SystemUserEntity.builder().username("admin").build();
+    when(jpaRepository.findByUsername(entity.getUsername())).thenReturn(Optional.of(entity));
+    var expected = SystemUser.builder().username(entity.getUsername()).build();
+    var actual = repository.findByUsername(entity.getUsername());
+
+    Assertions.assertTrue(actual.isPresent());
+    Assertions.assertEquals(expected, actual.get());
+  }
+
+  @Test
+  void should_trows_database_error_when_repository_fails() {
+    when(jpaRepository.findByUsername(any())).thenThrow(new RuntimeException("Some database error"));
+
+    var error = Assertions.assertThrows(DatabaseException.class, () -> repository.findByUsername("username"));
+    Assertions.assertEquals("Error de base de datos", error.getMessage());
   }
 }
