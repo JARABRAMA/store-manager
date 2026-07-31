7426813a7f8cb2f4996432bba3350082a6e5ec2f
brayan <brayanjaraba2006@gmail.com>
2026-07-31 16:26:11 -0500

    feat: login use case

---
+++ src/main/java/com/jarabrama/store_manager/authentication/usecases/LoginUseCase.java
@@ -0,0 +1,30 @@
+package com.jarabrama.store_manager.authentication.usecases;
+
+import com.jarabrama.store_manager.authentication.domain.SystemUserRepositoryPort;
+import com.jarabrama.store_manager.authentication.domain.exceptions.InvalidCredentialsException;
+import com.jarabrama.store_manager.authentication.presentation.dto.LoginRequest;
+import com.jarabrama.store_manager.authentication.presentation.dto.LoginResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class LoginUseCase {
+  private final SystemUserRepositoryPort userRepo;
+  private final PasswordEncoder passwordEncoder;
+
+  public LoginResponse execute(LoginRequest request) {
+    var user = userRepo.findByUsername(request.username())
+            .orElseThrow(() -> new InvalidCredentialsException("Credenciales incorrectas"));
+
+    var correctPassword = passwordEncoder.matches(request.password(),
+            user.getPasswordHash());
+
+    if(!correctPassword) {
+      throw new InvalidCredentialsException("Credenciales incorrectas");
+    }
+
+    return null;
+  }
+}
