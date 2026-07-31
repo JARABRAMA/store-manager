9cd3ab40e525e896833a4a5a8edd047d3cd769c5
brayan <brayanjaraba2006@gmail.com>
2026-07-31 15:55:06 -0500

    feat: session repository port

---
+++ src/main/java/com/jarabrama/store_manager/authentication/domain/SessionRepositoryPort.java
@@ -0,0 +1,10 @@
+package com.jarabrama.store_manager.authentication.domain;
+
+import com.jarabrama.store_manager.authentication.domain.model.Session;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface SessionRepositoryPort {
+
+}
