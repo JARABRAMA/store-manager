1837d3bf607ba588e7db1a89d468c98852a14bf4
brayan <brayanjaraba2006@gmail.com>
2026-08-08 11:43:08 -0500

    chore: optimize imports

--- src/main/java/com/jarabrama/store_manager/authentication/service/JwtService.java
+++ src/main/java/com/jarabrama/store_manager/authentication/service/JwtService.java
@@ -1,7 +1,6 @@
 package com.jarabrama.store_manager.authentication.service;
 
-import com.jarabrama.store_manager.authentication.domain.model.AuthToken;
-import com.jarabrama.store_manager.authentication.service.model.NewJwtTokenRequest;
+ import com.jarabrama.store_manager.authentication.service.model.NewJwtTokenRequest;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
@@ -10,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
-import java.time.Duration;
 import java.time.Instant;
 import java.util.Date;
 import java.util.HashMap;
