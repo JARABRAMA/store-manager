5529a358c7ebf886d7404b4b85c39ce5518b70d4
brayan <brayanjaraba2006@gmail.com>
2026-08-08 12:15:21 -0500

    fix: find trusted device by user and hash

--- src/main/java/com/jarabrama/store_manager/authentication/domain/TrustedDeviceRepositoryPort.java
+++ src/main/java/com/jarabrama/store_manager/authentication/domain/TrustedDeviceRepositoryPort.java
@@ -3,9 +3,10 @@ package com.jarabrama.store_manager.authentication.domain;
 import com.jarabrama.store_manager.authentication.domain.model.TrustedDevice;
 
 import java.util.Optional;
+import java.util.UUID;
 
 public interface TrustedDeviceRepositoryPort {
 
-  Optional<TrustedDevice> findByDeviceHash(String deviceHash);
-  void saveNewTrustedDevice(TrustedDevice trustedDevice);
+  Optional<TrustedDevice> findByDeviceHashAndUserId(String deviceHash, UUID userId);
+  TrustedDevice save(TrustedDevice trustedDevice);
 }
