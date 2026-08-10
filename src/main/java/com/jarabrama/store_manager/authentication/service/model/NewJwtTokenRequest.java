bda7833c7bf2027930940623ecba707a879d485a
brayan <brayanjaraba2006@gmail.com>
2026-08-10 15:13:09 -0500

    fix: request to accept instant of the expiration timeout

--- src/main/java/com/jarabrama/store_manager/authentication/service/model/NewJwtTokenRequest.java
+++ src/main/java/com/jarabrama/store_manager/authentication/service/model/NewJwtTokenRequest.java
@@ -4,10 +4,10 @@ import com.jarabrama.store_manager.authentication.domain.model.AuthTokenType;
 import com.jarabrama.store_manager.authentication.domain.model.SystemRole;
 import lombok.Builder;
 
-import java.time.Duration;
+import java.time.Instant;
 import java.util.UUID;
 
 @Builder
 public record NewJwtTokenRequest(String username, SystemRole userRole, UUID sessionId, UUID trustedDeviceId,
-                                 AuthTokenType tokenType, Duration expirationTimeout) {
+                                 AuthTokenType tokenType, Instant expirationTime) {
 }
