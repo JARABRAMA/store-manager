33ccf2a7a43fe9ce34fc06a627d7c6b628f2cbaf
brayan <brayanjaraba2006@gmail.com>
2026-08-10 15:33:38 -0500

    fix: rename property from authToken to refreshToken

--- src/main/java/com/jarabrama/store_manager/authentication/domain/model/Session.java
+++ src/main/java/com/jarabrama/store_manager/authentication/domain/model/Session.java
@@ -1,9 +1,7 @@
 package com.jarabrama.store_manager.authentication.domain.model;
 
-import com.jarabrama.store_manager.authentication.domain.exceptions.SessionException;
 import lombok.*;
 
-import java.time.Duration;
 import java.time.Instant;
 import java.util.UUID;
 
@@ -12,16 +10,11 @@ import java.util.UUID;
 @Builder
 @Data
 public class Session {
-  private static final Duration TRUSTED_TIMEOUT = Duration.ofDays(5);
-  private static final Duration UNTRUSTED_TIMEOUT = Duration.ofMinutes(15);
-
   private final UUID id;
   private final UUID userId;
   private final UUID trustedDeviceId;
   private Instant lastActivityAt;
   private Instant expiresAt;
   private boolean revoked;
-  private AuthToken authToken;
-
-
+  private AuthToken refreshToken;
 }
