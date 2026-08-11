6721f4dd09d10710a3093a2774fc873fb95df7cf
brayan <brayanjaraba2006@gmail.com>
2026-08-11 16:41:07 -0500

    feat: remove unused function and adds a function that verifies if a token was singed with my secret key

--- src/main/java/com/jarabrama/store_manager/authentication/service/JwtService.java
+++ src/main/java/com/jarabrama/store_manager/authentication/service/JwtService.java
@@ -1,6 +1,6 @@
 package com.jarabrama.store_manager.authentication.service;
 
- import com.jarabrama.store_manager.authentication.service.model.NewJwtTokenRequest;
+import com.jarabrama.store_manager.authentication.service.model.NewJwtTokenRequest;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
@@ -44,11 +44,12 @@ public class JwtService {
     return Keys.hmacShaKeyFor(bites);
   }
 
-  public Claims getClaimsFromToken(String token) {
-    return Jwts.parser()
-            .verifyWith(getSignInKey())
-            .build()
-            .parseSignedClaims(token)
-            .getPayload();
+  public boolean isSinged(String tokenHash) {
+    try {
+      Jwts.parser().verifyWith(getSignInKey()).build().parseSignedClaims(tokenHash);
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
   }
 }
